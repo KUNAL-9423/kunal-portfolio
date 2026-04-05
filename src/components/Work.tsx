0a60304ee573f2834c15801f885761858bdbe527
KUNAL SOLANKI <hinatasolanki1@gmail.com>
2026-04-05 23:40:33 +0530

    Fix build errors

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback } from "react";
 import "./styles/Work.css";
 import WorkImage from "./WorkImage";
 import { MdArrowBack, MdArrowForward } from "react-icons/md";
